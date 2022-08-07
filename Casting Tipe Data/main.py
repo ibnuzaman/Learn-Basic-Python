4fe0a697e3b4c21f125e5a437178c0d75cefbc74
ibnuzaman <ibnuzaman2133@gmail.com>
2022-08-07 21:24:01 +0700

    update

--- Casting Tipe Data/main.py
+++ Casting Tipe Data/main.py
@@ -1,5 +1,27 @@
 # Belajar Casting
 # Merubah dari satu tipe ke tipe lain
 
-x_int = 0
-print("Data ini adalah ", type(x_int),"Yang bernilai",x_int)
+from os import system
+
+
+data_int = 0
+system("cls")
+print("Data ini adalah ", type(data_int),"Yang bernilai",data_int)
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("Data = ",data_float,",type =",type(data_float))
+print("Data = ",data_str,",type =",type(data_str))
+print("Data = ",data_bool,",type =",type(data_bool))
+
+print("---STRING---")
+data_str = "";
+print ("Data = ",data_str,"type = ",type(data_str))
+
+data_int = int(data_str) # Harus angka
+data_float = float(data_str) #harus angka
+data_bool = bool(data_str) #false jika string kosong
+print ("Data = ",data_int,"type = ",type(data_int))
+print ("Data = ",data_float,"type = ",type(data_float))
+print ("Data = ",data_bool,"type = ",type(data_bool))
---
+++ Casting Tipe Data/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+data_int),"Yang bernilai",data_int)
